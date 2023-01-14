d64b0014d853e8be2b4f54e8d00a460ae0d9258c
thesilvaprof <fabio01@protonmail.ch>
2023-01-14 15:23:08 -0300

    Da primeira a terceira aul, classes orient a obj

--- RUBY/RB_OBJETOS/FEITO-conta.rb
+++ RUBY/RB_OBJETOS/FEITO-conta.rb
@@ -2,28 +2,30 @@ class Conta
     attr_reader :numero, :titular
     attr_accessor :saldo
 
-   def initialize(numero, titular, saldo)
-    @numero = numero
-    @titular = titular
-    @saldo = saldo
+
+    def initialize(numero, titular, saldo)
+        @numero = numero
+        @titular = titular
+        @saldo = saldo
     end
 
     def sacar(valor)
         if saldo >= valor
             self.saldo -= valor
         else
-            puts "Saldo indisponivel."
+            puts "Saldo insuficiente."
         end
-        
     end
+
     def depositar(valor)
         self.saldo += valor
     end
+    
     def transferir(conta_destino, valor)
         sacar(valor)
-        conta_destino.depositar(valor)        
+        conta_destino.depositar(valor)
+        
     end
-     
     
-
+    
 end
