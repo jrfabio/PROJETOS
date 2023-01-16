99263369a1c4a6f84a91621bb85b0cf3042bae24
thesilvaprof <fabio01@protonmail.ch>
2023-01-16 09:29:13 -0300

    Classes, objetos, métodos, associação e herança RB

--- RUBY/RB_OBJETOS/conta.rb
+++ RUBY/RB_OBJETOS/conta.rb
@@ -1,29 +1,35 @@
+require_relative 'cliente'
+
 class Conta
     attr_reader :numero, :titular
     attr_accessor :saldo
 
-   def initialize(numero, titular, saldo)
-    @numero = numero
-    @titular = titular
-    @saldo = saldo
+    def initialize(numero, titular, saldo)
+        @numero = numero
+        @titular = titular
+        @saldo = saldo
     end
 
     def sacar(valor)
         if saldo >= valor
             self.saldo -= valor
         else
-            puts "Saldo indisponivel."
+            puts "Saldo insufuciente."
         end
-        
     end
+
     def depositar(valor)
         self.saldo += valor
     end
-    def transferir(conta_destino, valor)
-        sacar(valor)
-        conta_destino.depositar(valor)        
-    end
-     
-    
 
+    def transferir(conta_destino, valor)
+        if saldo >= valor
+            sacar(valor)
+            conta_destino.depositar(valor)
+        else
+            puts "Saldo insufuciente."
+        end
+    end  
+
+    
 end
