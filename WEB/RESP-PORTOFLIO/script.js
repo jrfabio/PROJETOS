3c02665048bd2c52a04ffa082899a32a9b92b874
thesilvaprof <fabio01@protonmail.ch>
2023-03-09 10:50:33 -0300

    Finalizado porém, não está chamando o js

---
+++ WEB/RESP-PORTOFLIO/script.js
@@ -0,0 +1,9 @@
+import Typed from 'typed.js';
+
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer now", "Not YouTuber", "Not Blogger"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
