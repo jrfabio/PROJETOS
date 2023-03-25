7ce4048358499fe14044dc262f7aea4d02fc89ae
thesilvaprof <fabio01@protonmail.ch>
2023-03-24 21:58:41 -0300

    Start, e nada contece.

---
+++ WEB/cronometro/index.js
@@ -0,0 +1,42 @@
+const timeDisplay = document.querySelector("#time-display");
+const startBtn = document.querySelector("#startBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
+const resetBtn = document.querySelector("#resetBtn");
+
+let startTime = 0;
+let elapsedTime = 0;
+let currentTime = 0;
+let paused = true;
+let intervalId;
+let hrs = 0;
+let mins = 0;
+let secs = 0;
+
+startBtn.addEventListener("click", () => {
+    if (paused) {
+        paused = false;
+        startTime = Date.now() - elapsedTime;
+        intervalId = setInterval(updateTime, 75);
+    }
+});
+pauseBtn.addEventListener("click", () => {});
+resetBtn.addEventListener("click", () => {});
+
+function updateTime() {
+    elapsedTime = Date.now() - startTime;
+    
+    secs = Math.floor((elapsedTime / 1000) % 60);
+    mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
+    hrs = Math.floor((elapsedTime / (1000 * 60 * 60)) % 60);
+
+
+    secs = pad(secs);
+    mins = pad(mins);
+    hrs = pad(hrs);
+    timeDisplay.textContent = `${hrs}:${mins}:${secs}`;
+
+
+    function pad(unit) {
+        return (("0") + unit).length > 2 ? unit : "0" + unit;
+    }
+}
